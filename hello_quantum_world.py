317eea7aa1f0207ff1869d696073e711a9fcf73b
Ayesha <32429595+bladedancer-256@users.noreply.github.com>
2018-11-25 22:11:48 +1100

    Create hello_quantum_world.py
    I'm still confused about what this does but hey it works lol

---
+++ hello_quantum_world.py
@@ -0,0 +1,10 @@
+from qiskit import *
+q = QuantumRegister(2)
+c = ClassicalRegister(2)
+qc = QuantumCircuit(q,c)
+qc.h(q[0])
+qc.cx(q[0], q[1])
+qc.measure(q,c)
+backend_sim = Aer.get_backend('qasm_simulator_py')
+result = execute(qc, backend_sim).result()
+print(result.get_counts(qc))
